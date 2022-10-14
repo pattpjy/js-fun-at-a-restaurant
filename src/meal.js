e7b5c15e4107e5d3bf248446e4ad139f571c7570
Patt Sookmark <pijiya@sookmark.com>
2022-10-13 23:12:37 -0600

    Complete meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  var menuItemName =  `Delicious ${name}`
+   return menuItemName
+}
+function createMenuItem(name,price,type){
+  var menuItem = {
+    name: name,
+    price: price,
+    type:type,
+  };
+  return menuItem
 }
 
 
+function addIngredients(items,ingredients){
+  if (ingredients.indexOf(items) >=0) {
+    return;
+  } else {
+    ingredients.push(items);
+  }
+}
+
+function formatPrice(initialPrice){
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(reduce){
+  return reduce*(1- 0.1);
+}
+
+function createRecipe(title,ingredients, menuItemType){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  };
+  return recipe
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
